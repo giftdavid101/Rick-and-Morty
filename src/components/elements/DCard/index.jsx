313f6974d0913ed75fbea1ec83abba8192887094
giftdavid101 <giftdavid101@gmail.com>
2021-01-21 13:38:53 +0100

    DeadCharacters component fixed

---
+++ src/components/elements/DCard/index.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from 'react';
+import {useHistory} from "react-router-dom";
+import Info from "../../Info";
+import './dcard.style.css'
+
+const DCard = ({children, image,status,gender,origin, data,id}) => {
+    const [showInfo, setShowInfo] = useState(false)
+    const history = useHistory()
+    // console.log({data})
+
+    const handleClick = () => {
+        setShowInfo(true)
+        history.push('/characters/single-characters', {...data})
+    }
+    return (
+        <div>
+            <div  className="card-img" onClick={handleClick}>
+                <img alt="character" src={image}/>
+            </div>
+            <div className={"card-meta"}>
+                {children}
+            </div>
+
+            {
+                showInfo ? <Info>
+                    <div>Status: {status}</div>
+                    <div>Gender: {gender}</div>
+                    <div>Origin:{origin}</div>
+                </Info>:null
+            }
+        </div>
+    );
+};
+
+export default DCard;
