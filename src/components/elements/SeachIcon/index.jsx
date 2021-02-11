aa89a04419f037f0ddd68d1505d987a6c86c40e2
giftdavid101 <giftdavid101@gmail.com>
2021-02-11 01:14:40 +0100

    added close functionality on searchbar

--- src/components/elements/SeachIcon/index.jsx
+++ src/components/elements/SeachIcon/index.jsx
@@ -5,15 +5,17 @@ import InputBar from "../../compounds/Inputbar";
 import {AiOutlineClose} from "react-icons/all";
 
 const SearchIcon = ({clickSearchIcon,setClickSearchIcon,searchCharacters,setInput, filtered}) => {
+
  const handleClickSearch = () => setClickSearchIcon(true)
+    const closeInputBar = () => setClickSearchIcon(false)
 
     return (
         <div className='search-icon'>
             {
                clickSearchIcon ?
-                   <div style={{width:'100%', background:'#861931' , display:'flex',justifyContent:'center', alignItems:'center',height:'80px'}}>
+                   <div style={{width:'100%' , display:'flex',justifyContent:'center', alignItems:'center'}}>
                      <InputBar setInput={setInput} filtered={filtered} searchCharacters={searchCharacters}/>
-                     <div style={{color:'#1f0202', fontSize:'25px'}}><AiOutlineClose/></div>
+                     <div style={{color:'#1f0202', fontSize:'25px'}} onClick={closeInputBar}><AiOutlineClose/></div>
                   </div>
                   :
                   <span  style={{fontSize:"30px"}} onClick={handleClickSearch}><RiSearch2Line/></span>
